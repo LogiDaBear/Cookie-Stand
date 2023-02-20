8907698adf421e329729d7e2708e665cca5fc634
Logan Reese <lreese2112@gmail.com>
2023-02-20 01:55:22 +0000

    css styling

--- js/app.js
+++ js/app.js
@@ -33,15 +33,17 @@ function City(location, minCust, maxCust, avgcookieSale){
   this.dailyTotal = 0;
 }
 
-//*************Helper Functions/Utilities for random customer Lab 7 */
 
+
+//*************Helper Functions/Utilities for random customer Lab 7 */
 
 function randomCust(min,max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//************Prototype Methods Lab 7 */
 
+
+//************Prototype Methods Lab 7 */
 
 City.prototype.getdailyTotal = function(){
       for (let i = 0; i < hours.length; i++) {
@@ -158,6 +160,8 @@ function handleFormSubmit(event){
   let standAvg = event.target.standAvg.value;
 
   let newStand = new City (standCountry, standMin, standMax, standAvg);
+
+  
   citySales.push(newStand);
 
 
